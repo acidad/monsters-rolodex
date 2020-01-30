ece83531a5bb3337c193eda45ffe7a170d7672c2
acidad <43663526+acidad@users.noreply.github.com>
2020-01-30 15:01:37 +0800

    Updates

---
+++ precache-manifest.5713c7a5c323a113679de79fd7337a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3caef9b417401f637fdb738449a302",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ed2a5da5f4fa94c4713",
+    "url": "/monsters-rolodex/static/css/main.5ca64957.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ed2a5da5f4fa94c4713",
+    "url": "/monsters-rolodex/static/js/main.9d9b7ea8.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
